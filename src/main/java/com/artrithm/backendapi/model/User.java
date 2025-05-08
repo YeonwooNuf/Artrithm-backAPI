23786ad1a354bfd54d1f5ee887c0624ca6c238d6
장연우 <jangyw0927@naver.com>
2025-05-08 23:57:10 +0900

    // User 엔티티 생성

---
+++ src/main/java/com/artrithm/backendapi/model/User.java
@@ -0,0 +1,33 @@
+package com.artrithm.backendapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // 고유 식별자
+
+    @Column(nullable = false, unique = true)
+    private String loginId;  // 회원가입용 ID
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+}
