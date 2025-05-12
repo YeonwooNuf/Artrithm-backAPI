5ff84b54986bde82d0f56057a381821514e5fd5c
장연우 <jangyw0927@naver.com>
2025-05-12 18:41:41 +0900

    // uploads 디렉토리 파일 접근 허용

--- src/main/java/com/artrithm/backendapi/config/WebConfig.java
+++ src/main/java/com/artrithm/backendapi/config/WebConfig.java
@@ -1,16 +1,23 @@
 package com.artrithm.backendapi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173") // React dev 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true); // 세션 쿠키 허용
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/"); // 프로젝트 루트 기준 폴더
     }
 }
