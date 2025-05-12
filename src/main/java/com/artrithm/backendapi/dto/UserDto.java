c96533431df53aaa2b420c51c1187bf1496ec63a
장연우 <jangyw0927@naver.com>
2025-05-12 18:43:42 +0900

    // 이미지 파일 및 경로 분할

--- src/main/java/com/artrithm/backendapi/dto/UserDto.java
+++ src/main/java/com/artrithm/backendapi/dto/UserDto.java
@@ -2,6 +2,9 @@ package com.artrithm.backendapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
@@ -15,11 +18,14 @@ public class UserDto {
     private String loginId;
     private String password;
     private String nickname;
+    private LocalDate birth;
     private String email;
     private String phoneNumber;
 
     private String role;               // USER / ARTIST / ADMIN
     private Boolean isArtistApproved; // 작가 승인 여부
     private String artistBio;         // 작가 소개글
-    private String profileImage;      // 사용자 공통 프로필 이미지
+    private String profileImage;      // S3 경로 또는 URL
+
+    private MultipartFile profileImageFile; // ✅ 실제 이미지 파일
 }
