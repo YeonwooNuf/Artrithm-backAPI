5491a44b22867875cab327bee6664ddda40f6246
장연우 <jangyw0927@naver.com>
2025-06-11 10:51:40 +0900

    // 작품 정보 전송 fromEntity 생성

--- src/main/java/com/artrithm/backendapi/dto/ArtworkDto.java
+++ src/main/java/com/artrithm/backendapi/dto/ArtworkDto.java
@@ -1,5 +1,6 @@
 package com.artrithm.backendapi.dto;
 
+import com.artrithm.backendapi.model.Artwork;
 import lombok.*;
 
 @Getter
@@ -17,4 +18,24 @@ public class ArtworkDto {
     private String artistName; // 명화 작가일 경우
     private String userNickname; // 개인 작가일 경우
     private String explanationFileUrl; // 명화 설명용 PDF
+
+    public static ArtworkDto fromEntity(Artwork artwork) {
+        return ArtworkDto.builder()
+                .id(artwork.getId())
+                .title(artwork.getTitle())
+                .description(artwork.getDescription())
+                .imageUrl(artwork.getImageUrl())
+                .explanationFileUrl(artwork.getExplanationFileUrl()) // 명화 설명 PDF
+                .artistName(
+                        artwork.getArtist() != null
+                                ? artwork.getArtist().getName()
+                                : null
+                )
+                .userNickname(
+                        artwork.getUser() != null
+                                ? artwork.getUser().getNickname()
+                                : null
+                )
+                .build();
+    }
 }
