dbadd6da3d67a6f348c47fdbc80a53686ec2b5e2
장연우 <jangyw0927@naver.com>
2025-06-11 10:53:14 +0900

    // toDto -> fromEntity 사용

--- src/main/java/com/artrithm/backendapi/service/ExhibitionService.java
+++ src/main/java/com/artrithm/backendapi/service/ExhibitionService.java
@@ -1,5 +1,6 @@
 package com.artrithm.backendapi.service;
 
+import com.artrithm.backendapi.dto.ArtistDto;
 import com.artrithm.backendapi.dto.ArtworkDto;
 import com.artrithm.backendapi.dto.ExhibitionDto;
 import com.artrithm.backendapi.model.Artist;
@@ -187,25 +188,15 @@ public class ExhibitionService {
     }
 
     private ExhibitionDto toDto(Exhibition exhibition) {
-        return ExhibitionDto.builder()
-                .id(exhibition.getId())
-                .authorId(exhibition.getAuthor().getId())
-                .authorNickname(exhibition.getAuthor().getNickname())
-                .title(exhibition.getTitle())
-                .description(exhibition.getDescription())
-                .theme(exhibition.getTheme())
-                .thumbnailUrl(exhibition.getThumbnailUrl())
-                .keywords(exhibition.getKeywords())
-                .artworks(exhibition.getArtworks().stream().map(this::toArtworkDto).collect(Collectors.toList()))
-                .build();
+        return ExhibitionDto.fromEntity(exhibition); // ✅ 이렇게 변경!
     }
 
-    private ArtworkDto toArtworkDto(Artwork a) {
+    private ArtworkDto toArtworkDto(Artwork artwork) {
         return ArtworkDto.builder()
-                .id(a.getId())
-                .title(a.getTitle())
-                .description(a.getDescription())
-                .imageUrl(a.getImageUrl())
+                .id(artwork.getId())
+                .title(artwork.getTitle())
+                .description(artwork.getDescription())
+                .imageUrl(artwork.getImageUrl())
                 .build();
     }
 }
