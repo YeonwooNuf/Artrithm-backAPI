62908ed8d6ec42f631d4eb270ecec0a76e8be9f1
jinhakim01 <jinistar2222@gmail.com>
2025-06-16 17:35:34 +0900

    // 장바구니 테이블

---
+++ src/main/java/com/artrithm/backendapi/model/CartItem.java
@@ -0,0 +1,38 @@
+package com.artrithm.backendapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artwork_id", nullable = false)
+    private Artwork artwork;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CartItemType type; // FIXED_PRICE or AUCTION
+
+    @Column(nullable = false)
+    private LocalDateTime addedAt;
+
+    @Column(nullable = false)
+    private Integer price; // 고정가든 경매든 이곳에 저장
+
+
+}
