dd1b8fb0aa0dcb12e6dcea0061aee24288656690
jinhakim01 <jinistar2222@gmail.com>
2025-06-16 17:38:31 +0900

    // 상태로 경매 불러오기

--- src/main/java/com/artrithm/backendapi/repository/AuctionRepository.java
+++ src/main/java/com/artrithm/backendapi/repository/AuctionRepository.java
@@ -1,9 +1,16 @@
 package com.artrithm.backendapi.repository;
 
 import com.artrithm.backendapi.model.Auction;
+import com.artrithm.backendapi.model.AuctionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    List<Auction> findByStatus(AuctionStatus status);
+    boolean existsByStatus(AuctionStatus status);
+    Optional<Auction> findFirstByStatus(AuctionStatus status);
+    Optional<Auction> findByArtworkId(Long artworkId);
+
 }
