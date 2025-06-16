9cb8e4140e456f9b375b4b005cb6b54f92583d61
장연우 <jangyw0927@naver.com>
2025-06-16 16:13:18 +0900

    // 채팅 메세지 조회용 메소드

---
+++ src/main/java/com/artrithm/backendapi/repository/ChatMessageRepository.java
@@ -0,0 +1,13 @@
+package com.artrithm.backendapi.repository;
+
+import com.artrithm.backendapi.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    Optional<ChatMessage> findTopByRoomIdOrderBySentAtDesc(String roomId);
+    void deleteByRoomId(String roomId);
+    List<ChatMessage> findByRoomIdOrderBySentAtAsc(String roomId);
+}
