b0f5e869495f7d75497dc984dc83d7889e31cf2f
장연우 <jangyw0927@naver.com>
2025-06-23 12:05:05 +0900

    // 장바구니 관련 주문 엔티티

---
+++ src/main/java/com/artrithm/backendapi/model/CartOrder.java
@@ -0,0 +1,32 @@
+package com.artrithm.backendapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime orderedAt;
+
+    @OneToMany(mappedBy = "cartOrder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartOrderItem> items;
+
+    // 주문 상태 등을 나중에 추가 가능 (예: COMPLETED, CANCELED 등)
+}
