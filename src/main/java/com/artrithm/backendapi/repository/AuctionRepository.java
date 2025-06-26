65e4aa2163fb510d768b8e7646f1d77adad95274
장연우 <jangyw0927@naver.com>
2025-06-26 11:34:21 +0900

    // 낙찰자 기준 조회 메소드

--- src/main/java/com/artrithm/backendapi/repository/AuctionRepository.java
+++ src/main/java/com/artrithm/backendapi/repository/AuctionRepository.java
@@ -8,13 +8,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+
     List<Auction> findByStatus(AuctionStatus status);
+
     boolean existsByStatus(AuctionStatus status);
+
     Optional<Auction> findFirstByStatus(AuctionStatus status);
+
     Optional<Auction> findByArtworkId(Long artworkId);
 
     Optional<Auction> findFirstByStatusOrderByStartTimeDesc(AuctionStatus auctionStatus);
 
     Optional<Auction> findFirstByStatusOrderByEndTimeDesc(AuctionStatus status);
 
+    List<Auction> findAllByWinnerUserId(String winnerUserId);
 }
