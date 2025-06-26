bc13f603f3a40ee7e3a8199904e001536d63d1d8
장연우 <jangyw0927@naver.com>
2025-06-26 20:17:47 +0900

    // 관리자 id 값 조회

--- src/main/java/com/artrithm/backendapi/repository/UserRepository.java
+++ src/main/java/com/artrithm/backendapi/repository/UserRepository.java
@@ -9,15 +9,18 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // ✅ 아이디 중복 확인
+    // 아이디 중복 확인
     boolean existsByLoginId(String loginId);
 
-    // ✅ 이메일 중복 확인
+    // 이메일 중복 확인
     boolean existsByEmail(String email);
 
-    // ✅ 전화번호 중복 확인
+    // 전화번호 중복 확인
     boolean existsByPhoneNumber(String phoneNumber);
 
-    // ✅ 로그인 시 아이디로 조회
+    // 로그인 시 아이디로 조회
     Optional<User> findByLoginId(String loginId);
+
+    // 관리자 id 값 role 기반으로 조회
+    Optional<User> findFirstByRole(User.Role role);
 }
