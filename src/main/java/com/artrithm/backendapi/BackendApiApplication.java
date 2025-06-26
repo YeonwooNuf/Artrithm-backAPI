c03551145a814a1344869b37c07eeea535a07ebb
장연우 <jangyw0927@naver.com>
2025-06-26 11:24:16 +0900

    // LocalDateTime 서버 시간 설정

--- src/main/java/com/artrithm/backendapi/BackendApiApplication.java
+++ src/main/java/com/artrithm/backendapi/BackendApiApplication.java
@@ -3,10 +3,14 @@ package com.artrithm.backendapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendApiApplication {
 
 	public static void main(String[] args) {
+		// 애플리케이션 전체 시간대를 Asia/Seoul로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(BackendApiApplication.class, args);
 	}
 
