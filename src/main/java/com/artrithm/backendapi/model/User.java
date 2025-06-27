3070c4562e18d8ece9820495a8c1a39a39c037fd
장연우 <jangyw0927@naver.com>
2025-06-27 18:03:02 +0900

    // 구독 정보 단방향 조회

--- src/main/java/com/artrithm/backendapi/model/User.java
+++ src/main/java/com/artrithm/backendapi/model/User.java
@@ -63,4 +63,7 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserSubscription> subscriptions = new ArrayList<>();
 }
